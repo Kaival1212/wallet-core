7ed4158548bd3d4112dfdfca1a4eebf67e415371
Kaival <kaival225@gmail.com>
2025-12-15 13:34:57 +0000

    final 2

--- src/Http/Controllers/AppleWalletApi.php
+++ src/Http/Controllers/AppleWalletApi.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\Controller as BaseController;
 
 
 
-class AppleWalletApi extends BaseController
+class AppleWalletApi extends BaseController 
 {
 
     function registerDevice($deviceId, $passTypeId, $serialNumber, Request $request)
